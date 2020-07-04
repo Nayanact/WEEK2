e47f6e0e990c57625ee30f41fe866ac0b608c63f
USER <USER@ASHIYANA>
2020-07-04 15:26:43 +0530

    Array Week 2 Assignment 1

---
+++ Array/src/ARRAY/array.java
@@ -0,0 +1,13 @@
+package ARRAY;
+
+public class array {
+	public static void main (String[] args) {
+		int arr[] = new int[40];
+		for(int i = 0;i<40;i++) {
+			arr[i] = 10 + i;
+		}
+		
+		System.out.println(arr[3]);
+	}
+
+}
