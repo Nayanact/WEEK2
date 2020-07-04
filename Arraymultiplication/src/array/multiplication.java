69c0197ed84deed6018a46478e066ab0289cea8b
USER <USER@ASHIYANA>
2020-07-04 15:19:27 +0530

    Array multiplication

---
+++ Arraymultiplication/src/array/multiplication.java
@@ -0,0 +1,12 @@
+package array;
+
+public class multiplication {
+	public static void main(String[] args) {
+		int num = 10;
+		System.out.println("Multiplication table of 10");
+		for(int i=1;i<=12;i++) {
+			System.out.printf("%d * %d = %d\n", num, i, num*i);
+		}
+	}
+
+}
