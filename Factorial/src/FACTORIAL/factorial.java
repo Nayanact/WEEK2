0443377e3cb04b1b05aecf494d8eeb4e772434b9
USER <USER@ASHIYANA>
2020-07-04 16:01:51 +0530

    Factorial Week 2 Assignment 2

---
+++ Factorial/src/FACTORIAL/factorial.java
@@ -0,0 +1,13 @@
+package FACTORIAL;
+
+public class factorial {
+	public static void main(String[] args) {
+		int num = 11;
+		long factorial = 1;
+		for (int i=1; i<=num;i++) {
+			factorial = factorial * i;
+			}
+		System.out.printf("Factorial of %d = %d", num, factorial);
+	}
+
+}
